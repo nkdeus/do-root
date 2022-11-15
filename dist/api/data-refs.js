2f0147694079d8267153293160abb6effd22b49f
nkdeus <nkdeus@gmail.com>
2022-11-15 19:30:00 +0100

    lunch

--- dist/api/data-refs.js
+++ dist/api/data-refs.js
@@ -1,10 +1,10 @@
 var refs = [
   {
-    "title":"refA",
-    "src":"refA.jpg"
+    "title":"fruit-1",
+    "src":"fruit-1.jpg"
   },
   {
-    "title":"refA",
-    "src":"refA.jpg"
+    "title":"fruit-2",
+    "src":"fruit-2.jpg"
   }
 ];
