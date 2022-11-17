55708c3b8b9a70d80f6845b96afa13267367d288
nkdeus <nkdeus@gmail.com>
2022-11-17 04:14:01 +0100

    lunch

--- dist/api/data-refs.js
+++ dist/api/data-refs.js
@@ -1,156 +1,160 @@
 var refs = [
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022",
     "love":"true"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022",
     "love":"true"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022",
     "love":"true"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022",
     "love":"true"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   },
   {
     "title":"fruit-4",
-    "src":"fruit-4.jpg",
+    "src":"fruit-4",
     "year":"2022"
   },
   {
     "title":"fruit-5",
-    "src":"fruit-5.jpg",
+    "src":"fruit-5",
     "year":"2022"
   },
   {
     "title":"fruit-8",
-    "src":"fruit-8.jpg",
+    "src":"fruit-8",
     "year":"2022"
   }
 ];
+
+module.exports = {
+  scripts: refs
+};
